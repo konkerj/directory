6d00c24fb0e1772cc99585a0156d163fd5a1ff22
xupan <xupan@benefitz.co.nz>
2018-06-14 09:49:19 +1200

    setup deployer.

---
+++ deploy.php
@@ -0,0 +1,26 @@
+<?php
+namespace Deployer;
+
+require 'recipe/silverstripe.php';
+
+// Project name
+set('application', 'my_project');
+
+// Project repository
+set('repository', 'https://jiangxupan%40gmail.com@github.com/konkerj/directory.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Hosts
+host('staging')
+    ->hostname('web1.b-web.co.nz')
+    ->user('xupan')
+    ->port(22)
+    //->configFile('~/.ssh/config')
+    ->identityFile('~/.ssh/id_rsa')
+    ->stage('staging')
+    ->set('deploy_path', '/var/test-dev');    
+    
+
+desc('Deploy your project');
